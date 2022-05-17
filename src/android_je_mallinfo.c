300fb3c5cae41cd32a718ca6af5a4d0c1042727d
Juhyung Park <qkrwngud825@gmail.com>
2022-05-18 03:24:35 +0900

    jemalloc: adjust Android extensions
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- src/android_je_mallinfo.c
+++ src/android_je_mallinfo.c
@@ -23,8 +23,8 @@ static size_t accumulate_large_allocs(arena_t* arena) {
    */
   for (unsigned j = 0; j < SC_NSIZES - SC_NBINS; j++) {
     /* Read ndalloc first so that we guarantee nmalloc >= ndalloc. */
-    uint64_t ndalloc = arena_stats_read_u64(TSDN_NULL, &arena->stats, &arena->stats.lstats[j].ndalloc);
-    uint64_t nmalloc = arena_stats_read_u64(TSDN_NULL, &arena->stats, &arena->stats.lstats[j].nmalloc);
+    uint64_t ndalloc = locked_read_u64(TSDN_NULL, LOCKEDINT_MTX(arena->stats.mtx), &arena->stats.lstats[j].ndalloc);
+    uint64_t nmalloc = locked_read_u64(TSDN_NULL, LOCKEDINT_MTX(arena->stats.mtx), &arena->stats.lstats[j].nmalloc);
     size_t allocs = (size_t)(nmalloc - ndalloc);
     total_bytes += sz_index2size(SC_NBINS + j) * allocs;
   }
@@ -33,15 +33,14 @@ static size_t accumulate_large_allocs(arena_t* arena) {
 
 static size_t accumulate_small_allocs(arena_t* arena) {
   size_t total_bytes = 0;
-  for (unsigned i = 0; i < SC_NBINS; i++) {
-		for (unsigned j = 0; j < bin_infos[i].n_shards; j++) {
-      bin_t* bin = &arena->bins[i].bin_shards[j];
+  unsigned binshard;
+  bin_t* bin;
+  for (unsigned j = 0; j < SC_NBINS; j++) {
+    bin = arena_bin_choose(TSDN_NULL, arena, j, &binshard);
 
-      /* NOTE: This includes allocations cached on every thread. */
-      malloc_mutex_lock(TSDN_NULL, &bin->lock);
-      total_bytes += bin_infos[j].reg_size * bin->stats.curregs;
-      malloc_mutex_unlock(TSDN_NULL, &bin->lock);
-		}
+    /* NOTE: This includes allocations cached on every thread. */
+    total_bytes += bin_infos[j].reg_size * bin->stats.curregs;
+    malloc_mutex_unlock(TSDN_NULL, &bin->lock);
   }
   return total_bytes;
 }
@@ -58,7 +57,7 @@ struct mallinfo je_mallinfo() {
   for (unsigned i = 0; i < narenas_auto; i++) {
     arena_t* arena = atomic_load_p(&arenas[i], ATOMIC_ACQUIRE);
     if (arena != NULL) {
-      mi.hblkhd += atomic_load_zu(&arena->stats.mapped, ATOMIC_ACQUIRE);
+      mi.hblkhd += atomic_load_zu(&arena->pa_shard.pac.stats->pac_mapped, ATOMIC_ACQUIRE);
 
       mi.uordblks += accumulate_small_allocs(arena);
       mi.uordblks += accumulate_large_allocs(arena);
@@ -86,7 +85,7 @@ struct mallinfo je_mallinfo_arena_info(size_t aidx) {
   if (aidx < narenas_auto) {
     arena_t* arena = atomic_load_p(&arenas[aidx], ATOMIC_ACQUIRE);
     if (arena != NULL) {
-      mi.hblkhd = atomic_load_zu(&arena->stats.mapped, ATOMIC_ACQUIRE);
+      mi.hblkhd = atomic_load_zu(&arena->pa_shard.pac.stats->pac_mapped, ATOMIC_ACQUIRE);
       mi.ordblks = accumulate_large_allocs(arena);
       mi.fsmblks = accumulate_small_allocs(arena);
     }
@@ -103,15 +102,13 @@ struct mallinfo je_mallinfo_bin_info(size_t aidx, size_t bidx) {
   if (aidx < narenas_auto && bidx < SC_NBINS) {
     arena_t* arena = atomic_load_p(&arenas[aidx], ATOMIC_ACQUIRE);
     if (arena != NULL) {
-      for (unsigned j = 0; j < bin_infos[bidx].n_shards; j++) {
-        bin_t* bin = &arena->bins[bidx].bin_shards[j];
+      unsigned binshard;
+      bin_t* bin = arena_bin_choose(TSDN_NULL, arena, (szind_t)bidx, &binshard);
 
-        malloc_mutex_lock(TSDN_NULL, &bin->lock);
-        mi.ordblks += bin_infos[bidx].reg_size * bin->stats.curregs;
-        mi.uordblks += (size_t) bin->stats.nmalloc;
-        mi.fordblks += (size_t) bin->stats.ndalloc;
-        malloc_mutex_unlock(TSDN_NULL, &bin->lock);
-      }
+      mi.ordblks = bin_infos[bidx].reg_size * bin->stats.curregs;
+      mi.uordblks = (size_t) bin->stats.nmalloc;
+      mi.fordblks = (size_t) bin->stats.ndalloc;
+      malloc_mutex_unlock(TSDN_NULL, &bin->lock);
     }
   }
   malloc_mutex_unlock(TSDN_NULL, &arenas_lock);
